49a1d3ce316b5b96055d484049f88df64f685f49
Galo1337 <galo.jimenez@ucsp.edu.pe>
2021-04-23 14:00:38 -0500

    Version 3
    La logica es la misma, pero en el modular tuve que poner un difusor que si los valores era el mismo pero uno poistivo y el otro negativo me tenia que retornar un 0 por defecto, cosa que cambie. Ademas corte parte del codigo para solo poner un string con las minusculas y mayusculas.

--- Cifrado Cesar v2/Cifrado Cesar v2/Cifrado Cesar v2.cpp
+++ Cifrado Cesar v2/Cifrado Cesar v2/Cifrado Cesar v2.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int encriptar(string mensaje, int a);
 int desencriptar(string mensaje, int a);
 int modular_galo(int a1, int b1);
+int NEO_hack(string mensaje_por_romper);
+int revision(string a, int x);
 
 int main() {
 	int x;
@@ -16,35 +18,18 @@ int main() {
 	string a;
 	getline(cin, a);
 
-	int choice;
-	cout << "Ingrese 1 is quiere encriptar, 2 si quiere decriptar: ";
-	cin >> choice;
-
-	if (choice == 1) {
-		encriptar(a, x);
-	}
-	else {
-		desencriptar(a, x);
-	}
+	revision(a, x);
 }
 
 int encriptar(string mensaje, int a) {
 
-	string alphaL = "abcdefghijklmnopqrstuvwxyz";
-	string alphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string alphaL = "abcdefghijklmnopqrstuvwxyz1234567890-,.()ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 	int mensage_l = mensaje.length();
 	for (int i = 0; i < mensage_l; i++) {
-		for (int j = 0; j < 26; j++) {
+		for (int j = 0; j < alphaL.length(); j++) {
 			if (mensaje[i] == alphaL[j]) {
-				int result = modular_galo(j+a,26);
+				int result = modular_galo(j + a, alphaL.length());
 				mensaje[i] = alphaL[result];
-				break;
-			}
-		}
-		for (int j = 0; j < 26; j++) {
-			if (mensaje[i] == alphaU[j]) {
-				int result = modular_galo(j + a, 26);
-				mensaje[i] = alphaU[result];
 				break;
 			}
 		}
@@ -52,25 +37,19 @@ int encriptar(string mensaje, int a) {
 	}
 	cout << mensaje;
 	return 1;
+	cout << mensaje;
+	return 1;
 }
 
 int desencriptar(string mensaje, int a) {
 
-	string alphaL = "abcdefghijklmnopqrstuvwxyz";
-	string alphaU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	string alphaL = "abcdefghijklmnopqrstuvwxyz1234567890-,.()ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
 	int mensage_l = mensaje.length();
 	for (int i = 0; i < mensage_l; i++) {
-		for (int j = 0; j < 26; j++) {
+		for (int j = 0; j < alphaL.length(); j++) {
 			if (mensaje[i] == alphaL[j]) {
-				int result = modular_galo(j - a, 26);
+				int result = modular_galo(j - a, alphaL.length());
 				mensaje[i] = alphaL[result];
-				break;
-			}
-		}
-		for (int j = 0; j < 26; j++) {
-			if (mensaje[i] == alphaU[j]) {
-				int result = modular_galo(j - a, 26);
-				mensaje[i] = alphaU[result];
 				break;
 			}
 		}
@@ -84,7 +63,10 @@ int modular_galo(int a1, int b1) {
 	int q = 0;
 	int r = 0;
 
-	if (a1 > 0) {
+	if (a1 == b1) {
+		return 0;
+	}
+	else if (a1 > 0) {
 		while (a1 > b1) {
 			a1 = a1 - b1;
 			q++;
@@ -100,4 +82,49 @@ int modular_galo(int a1, int b1) {
 	}
 
 	return r;
+}
+
+int NEO_hack(string mensaje_por_romper) {
+	bool done = 0;
+	while (done != 1) {
+		for (int i = 0; i < 26; i++) {
+			cout << "Este mensaje tiene sentido? \n";
+			desencriptar(mensaje_por_romper, i);
+			cout << "\n";
+			string confirmacion;
+			getline(cin, confirmacion);
+			if (confirmacion == "si") {
+				done = 1;
+				cout << "La clave usada en este codigo es: " << i;
+				return 1;
+			}
+		}
+		cout << "Esta seguro que el mensaje esta bien encriptado?";
+		done = 1;
+		return 1;
+	}
+	return 1;
+}
+
+int revision (string a, int x) {
+
+	cout << "Ingrese '1' si quiere encriptar, '2' si quiere decriptar \no '3' si quiere romper un cifrado ";
+	int choice;
+	cin >> choice;
+	cin.ignore();
+
+	if (choice == 1) {
+		encriptar(a, x);
+	}
+	else if (choice == 2) {
+		desencriptar(a, x);
+	}
+	else if (choice == 3) {
+		NEO_hack(a);
+	}
+	else {
+		cout << "Ingrese un numero correcto\n";
+		revision(a,x);
+	}
+	return 1;
 }
